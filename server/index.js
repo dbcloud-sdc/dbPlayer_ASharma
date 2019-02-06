10599ab7fee0fd5b7eddb24bc549d3a99fc8dce3
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-02-06 12:03:48 -0800

    Initial set-up

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 2222;
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.listen(PORT, () => {
+  console.log(`NOW LISTENING ON PORT ${PORT}`)
+});
