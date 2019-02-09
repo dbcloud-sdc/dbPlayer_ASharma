4db138a5a34d5133c168bfaee8ace8548d04baf3
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-02-09 14:04:41 -0800

    Set-up data base and first api calls

--- server/db.js
+++ server/db.js
@@ -1,13 +1,10 @@
 const Sequelize = require('sequelize');
-const faker = require('faker');
-const { username, password } = require('./config.js');
+const { username, password } = require('../config.js');
 
 const sequelize = new Sequelize('player', username, password, {
   host: 'localhost',
   dialect: 'mysql',
 });
-
-// sequelize.query('CREATE DATABASE player;');
 
 const Song = sequelize.define('songplayer', {
   id: {
@@ -54,23 +51,10 @@ const Song = sequelize.define('songplayer', {
   },
 });
 
+const getAllSongs = () => {
+  return Song.findAll();
+};
 
-Song.sync({ force: true }).then(() => {
-  const songs = [];
-  for (let i = 1; i <= 100; i += 1) {
-    const songMaker = Song.build({
-      songname: faker.random.words(),
-      artistname: faker.name.findName(),
-      imgurl: `https://s3-us-west-1.amazonaws.com/songimages/photos/simg${i}.jpg`,
-      hashtag: faker.random.words(),
-      timeelapsed: faker.date.past(),
-      starttime: 0,
-      songlength: faker.random.number(),
-      decibel: faker.random.number(),
-      comment: faker.lorem.sentence(),
-    }).save();
-    songs.push(songMaker);
-  }
-  return Promise.all(songs);
-})
-  .catch(err => console.log(err));
+module.exports = {
+  getAllSongs,
+};
--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
+const db = require('./db.js');
 
 const app = express();
 const PORT = 2222;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api/song_id', (req, res) => {
+  db.getAllSongs()
+    .then((song) => {
+      res.status(200).send(song);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`NOW LISTENING ON PORT ${PORT}`);
