3e9f9ac4469866a97103ebaddd36c6cd336c1ee0
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-02-08 16:42:26 -0800

    Make table using sequalize

--- schema.sql
+++ schema.sql
@@ -1,25 +1,26 @@
-DROP DATABASE IF EXISTS Player;
+-- DROP DATABASE IF EXISTS Player;
 
-CREATE DATABASE Player;
+-- CREATE DATABASE Player;
 
-USE Player;
+-- USE Player;
 
-CREATE TABLE Song (
- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- song_name TEXT NOT NULL,
- artist_name TEXT NOT NULL,
- song_url TEXT NOT NULL,
- img_url TEXT NOT NULL,
- hash_tag TEXT NOT NULL,
- start_time INT NOT NULL,
- song_length INT NOT NULL,
-);
+-- CREATE TABLE Song (
+--  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--  song_name TEXT NOT NULL,
+--  artist_name TEXT NOT NULL,
+--  song_url TEXT NOT NULL,
+--  img_url TEXT NOT NULL,
+--  hash_tag TEXT NOT NULL,
+--  start_time INT NOT NULL,
+--  song_length INT NOT NULL,
+--  decibel INT NOT NULL,
+-- );
 
-CREATE TABLE Comment (
- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- user_name TEXT NOT NULL,
- comment TEXT NOT NULL,
- img_url TEXT NOT NULL,
- time INT NOT NULL,
- Song_id INT NOT NULL FOREIGN KEY,
-);
+-- CREATE TABLE Comment (
+--  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--  user_name TEXT NOT NULL,
+--  comment TEXT NOT NULL,
+--  img_url TEXT NOT NULL,
+--  time INT NOT NULL,
+--  Song_id INT NOT NULL FOREIGN KEY,
+-- );
