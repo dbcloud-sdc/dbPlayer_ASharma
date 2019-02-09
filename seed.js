0914eabb17692fbb4337fce39165a56c98a7ba04
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-02-08 17:55:57 -0800

    Database set-up and populating tables

--- seed.js
+++ seed.js
@@ -11,45 +11,45 @@ const sequelize = new Sequelize('player', username, password, {
 
 const Song = sequelize.define('songplayer', {
   id: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.INTEGER(11),
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
   songname: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(100),
     allowNull: false,
   },
   artistname: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(32),
     allowNull: false,
   },
   imgurl: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(100),
     allowNull: false,
   },
   hashtag: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(100),
     allowNull: false,
   },
   timeelapsed: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.INTEGER(14),
     allowNull: false,
   },
   starttime: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.INTEGER(11),
     allowNull: false,
   },
   songlength: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.INTEGER(11),
     allowNull: false,
   },
   decibel: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.INTEGER(11),
     allowNull: false,
   },
   comment: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(200),
     allowNull: false,
   },
 });
@@ -57,18 +57,20 @@ const Song = sequelize.define('songplayer', {
 
 Song.sync({ force: true }).then(() => {
   const songs = [];
-  for (let i = 1; i < 101; i + 1) {
-    const songMaker = Song.build({
+  for (let i = 1; i <= 100; i += 1) {
+    let songMaker = Song.build({
       songname: faker.random.words(),
       artistname: faker.name.findName(),
       imgurl: `https://s3-us-west-1.amazonaws.com/songimages/photos/simg${i}.jpg`,
       hashtag: faker.random.words(),
-      timeelapsed: faker.date.past(),
+      timeelapsed: faker.random.number(),
       starttime: 0,
       songlength: faker.random.number(),
       decibel: faker.random.number(),
       comment: faker.lorem.sentence(),
     }).save();
+    console.log(songs.length);
+    console.log(songMaker);
     songs.push(songMaker);
   }
   return Promise.all(songs);
