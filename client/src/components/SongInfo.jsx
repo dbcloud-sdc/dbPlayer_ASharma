826dbd23e66428d7e08e079bba63020624d7f119
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-02-11 14:51:42 -0800

    Fix some styling

--- client/src/components/SongInfo.jsx
+++ client/src/components/SongInfo.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const SongInfo = (props) => {
   return (
     <div className="Info-Container">
-      <span className="Artist-Name">
+      <div className="Artist-Name">
       hello
-      </span>
+      </div>
       <span className="Song-Name">
       this is the name of the song and this is a test to make sure my text wraps
       </span>
