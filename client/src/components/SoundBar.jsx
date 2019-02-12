3c04af02f2d10174d0ed0116ba9d52a07b265433
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-02-12 01:35:05 -0800

    Sound bar now uses data from database

--- client/src/components/SoundBar.jsx
+++ client/src/components/SoundBar.jsx
@@ -1,38 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
 
-class SoundBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+// class SoundBar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
 
-    };
-  }
+//     };
+//     this.props.sound.decibel = this.props.sound.decibel.bind(this);
+//   }
 
-  render() {
-    const Bars = () => {
-      let barArray = [];
-      for (let i = 1; i < 200; i++) {
-        const randomNum = Math.floor(Math.random() * 10);
-        barArray.push(<span className={`Bar${randomNum}`} key={i} ></span>)
-      }
-      return barArray;
+//   render() {
+//     const Bars = () => {
+//       let barArray = [];
+//       for (let i = 1; i < 200; i++) {
+//         // const randomNum = Math.floor(Math.random() * 10);
+//         barArray.push(<span key={i} ></span>)
+//       }
+//       return barArray;
+//     }
+//     return (
+//       <div className="sound-container">
+//         <BarStyle sound={this.props.sound.decibel}>
+//           {Bars()}
+//         </BarStyle>
+//       </div>
+//     );
+//   }
+// }
+
+const SoundBar = (props) => {
+  const Bars = () => {
+    const barArray = [];
+    for (let i = 1; i < 200; i++) {
+      const randomNum = Math.floor(Math.random() * props.sound.decibel);
+      barArray.push(<BarStyleSpan style={{ height: randomNum }} key={i} />);
     }
-    return (
-      <div className="sound-container">
-        {Bars()}
-      </div>
-    );
-  }
-}
+    return barArray;
+  };
+  return (
+    <div className="sound-container">
+      {Bars()}
+    </div>
+  );
+};
 
 // const randomNum = {this.props.sound}
+// className={`Bar${randomNum}`}
 
-// const BarStyle = styled.div`
-//   width: 4px;
-//   height: 80px;
-//   background: white;
-//   margin-right: 1px;
-// `;
+const BarStyleSpan = styled.span`
+  width: 4px;
+  background: white;
+  margin-right: 1px;
+`;
 
 export default SoundBar;
