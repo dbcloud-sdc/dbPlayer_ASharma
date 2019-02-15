36b67047730eb019aa9bd7f5e483adcc3d09f3ef
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-02-15 13:31:35 -0800

    Ensure random number of comments per song

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -5,17 +5,26 @@ class Comment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      display: 0,
     };
+    this.handleHoverChange = this.handleHoverChange.bind(this);
+  }
+
+  handleHoverChange(index) {
+    this.setState({
+      display: index,
+    });
   }
 
   render() {
     const CommentImage = () => {
       const imgArray = [];
-      for (let i = 1; i < 21; i += 1) {
+      const randomNum = Math.floor(Math.random() * 21);
+      for (let i = 1; i < randomNum; i += 1) {
         imgArray.push(<CommentSpan
           key={i}
           style={{ backgroundImage: `url(${this.props.random[i]})` }}
+          onMouseEnter={() => this.handleHoverChange(i)}
         />);
       }
       return imgArray;
@@ -35,3 +44,10 @@ const CommentSpan = styled.span`
 `;
 
 export default Comment;
+
+
+//add text / username from random data
+//if hover is true color will = black if false color will be clear or whatever
+//can add conditional for timestamp as well
+
+//to get random number of comments.  generate a random number between 1-22 and put it in place of i < blank on span tage generation.  try it at 100 and see how it looks.  maybe they naturally overlap 
