d8d4936ad416a0b8594531fb638557c5bf0e715e
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-02-18 22:25:18 -0800

    Added some tests

--- test/comment.test.js
+++ test/comment.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import { configure, mount, shallow, render } from 'enzyme';
-import Comment from '../client/src/components/Comment';
+import {
+  configure, mount, shallow, render,
+} from 'enzyme';
 import toJson from 'enzyme-to-json';
+import Comment from '../client/src/components/Comment';
 
 configure({ adapter: new Adapter() });
 
@@ -11,22 +13,46 @@ describe('Comment Component', () => {
     imgurl: 'https://s3-us-west-1.amazonaws.com/songimages/photos/simg100.jpg',
     username: 'Annabelle Parker',
     comment: 'Nihil rerum possimus suscipit architecto non consequuntur provident adipisci.',
+    songlength: 281,
   };
   // it('should render Comment without throwing an error', () => {
-  //   const songcomment = shallow(<Comment random={props.imgurl} info={props.comment} name={props.username} />);
+  //   const songcomment = shallow(<Comment
+  //     random={props.imgurl}
+  //     info={props.comment}
+  //     name={props.username}
+  //     song={props.songlength}
+  //   />);
   //   expect(toJson(songcomment)).toMatchSnapshot();
+  //   songcomment.unmount();
   // });
   // it('should render correctly in "debug" mode', () => {
-  //   const debug = shallow(<Comment random={props.imgurl} info={props.comment} name={props.username} debug />);
+  //   const debug = shallow(<Comment
+  //     random={props.imgurl}
+  //     info={props.comment}
+  //     name={props.username}
+  //     song={props.songlength}
+  //     debug
+  //   />);
   //   expect(toJson(debug)).toMatchSnapshot();
+  //   songcomment.unmount();
   // });
   it('should mount correctly', () => {
-    const songcomment = mount(<Comment random={props.imgurl} info={props.comment} name={props.username} />);
+    const songcomment = mount(<Comment
+      random={props.imgurl}
+      info={props.comment}
+      name={props.username}
+      song={props.songlength}
+    />);
     expect(toJson(songcomment)).toMatchSnapshot();
     songcomment.unmount();
   });
   it('should render correctly', () => {
-    const songcomment = render(<Comment random={props.imgurl} info={props.comment} name={props.username} />);
+    const songcomment = render(<Comment
+      random={props.imgurl}
+      info={props.comment}
+      name={props.username}
+      song={props.songlength}
+    />);
     expect(toJson(songcomment)).toMatchSnapshot();
   });
 });
